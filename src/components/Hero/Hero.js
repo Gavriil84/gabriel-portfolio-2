6b8e90a70394ee0d9db3ca99504dfeee8710401c
Gavriil84 <gabwt.84@gmail.com>
2022-08-10 12:46:34 -0700

    Add aimation to text

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,6 +13,9 @@ const Hero = () => {
                     <Earth />
                 </Suspense>
             </Canvas>
+            <ScrollMsg>
+                <span >↓ Scroll Down for More ↓</span>
+            </ScrollMsg>
         </HeroContainer>
     )
 }
@@ -21,6 +24,24 @@ const HeroContainer = styled.div`
     width: 100%;
     height: 100%;
     position: relative;
+`
+
+const ScrollMsg = styled.div`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 99;
+    color: #fff;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 10px;
+    animation: bounce 0.8s infinite alternate; 
+
+    @keyframes bounce { 
+        0% { transform: translateY(0); } 
+        100% { transform: translateY(-20px); } 
+    }
+    
 `
 
 export default Hero
