d5d6ec8d27cacbe8eb67cda4c667a99a442d3e08
Gavriil84 <gabwt.84@gmail.com>
2022-08-11 12:32:02 -0700

    ajust

--- src/components/NavBar/Manu.js
+++ src/components/NavBar/Manu.js
@@ -50,7 +50,7 @@ const List = styled.ul`
         /* padding-top: 3.5rem; */
         transition: transform 0.3s ease-in-out;
         margin: 0;
-        box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 
         li{
             color: #fff;
